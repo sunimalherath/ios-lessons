45861534307af04feea74d93ea5811f1e5f72411
Sunimal Herath <sunimal@live.com>
2024-04-15 23:04:19 +1000

    stacking the buttons

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Sunimal Herath on 14/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            Color.teal
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag was tapped
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sunimal Herath on 14/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
