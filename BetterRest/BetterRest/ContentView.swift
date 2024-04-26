214635a43adf5cf288513dbafea60a105c5fcdd0
Sunimal Herath <sunimal@live.com>
2024-04-26 23:30:30 +1000

    simple stepper

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount) hours", value: $sleepAmount)
         }
         .padding()
     }
