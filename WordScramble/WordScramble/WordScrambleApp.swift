d5eb354a355fa22c68ab7b68983b24db9dd20bb3
Sunimal Herath <sunimal@live.com>
2024-05-08 23:22:48 +1000

    Initial commit for WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sunimal Herath on 8/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
