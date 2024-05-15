3eab05763813e960696a17c16d29683529de3ac9
Sunimal Herath <sunimal@live.com>
2024-05-15 23:05:41 +1000

    Adding different animation effects

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -14,18 +14,23 @@ struct ContentView: View {
     var body: some View {
         Button("Tap Me") {
             if toggleButton {
-                animationAmount -= 1
+                animationAmount -= 0.5
                 toggleButton = false
             } else {
-                animationAmount += 1
+                animationAmount += 0.5
                 toggleButton = true
             }
         }
         .padding(50)
         .background(.teal)
+        .foregroundStyle(.white)
         .clipShape(.circle)
         .scaleEffect(animationAmount)
-        .animation(.default, value: animationAmount)
+//        .animation(.default, value: animationAmount)
+//        .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+//        .animation(.easeInOut(duration: 2), value: animationAmount)
+//        .animation(.easeInOut(duration: 2).delay(0.5), value: animationAmount)
+        .animation(.easeInOut(duration: 2).repeatCount(3, autoreverses: true), value: animationAmount)
     }
 }
 
