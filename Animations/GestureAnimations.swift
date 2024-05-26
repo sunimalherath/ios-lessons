9dd4410db09f8dd2336f39ea9ccfa8c4d39873b3
Sunimal Herath <sunimal@live.com>
2024-05-26 23:15:10 +1000

    Adding drag gesture to gradient

--- Animations/GestureAnimations.swift
+++ Animations/GestureAnimations.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct GestureAnimations: View {
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
         LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
             .frame(width: 300, height: 200)
             .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged{ dragAmount = $0.translation }
+                    .onEnded { _ in dragAmount = .zero }
+            )
     }
 }
 
