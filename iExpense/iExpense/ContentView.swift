fea03ab78099f0394758d72d91b60ca5ff3c4808
Sunimal Herath <sunimal@live.com>
2024-05-30 22:53:19 +1000

    State with struct

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,13 +7,21 @@
 
 import SwiftUI
 
+struct User {
+    var firstName = "John"
+    var lastName = "Doe"
+}
+
+// struct is good when each view needs to have its own data.
+
 struct ContentView: View {
+    @State private var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("User name is \(user.firstName) \(user.lastName)")
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
         }
         .padding()
     }
