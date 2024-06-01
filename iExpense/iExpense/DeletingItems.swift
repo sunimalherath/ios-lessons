9a3416665d0302ea8893ac0807edff0b0a64f19a
Sunimal Herath <sunimal@live.com>
2024-06-01 22:28:52 +1000

    Deleting rows

--- iExpense/iExpense/DeletingItems.swift
+++ iExpense/iExpense/DeletingItems.swift
@@ -17,6 +17,7 @@ struct DeletingItems: View {
                 ForEach(numbers, id: \.self) {
                     Text("Row \($0)")
                 }
+                .onDelete(perform: removeRows)
             }
             
             Button("Add Number") {
@@ -24,6 +25,10 @@ struct DeletingItems: View {
                 currentNumber += 1
             }
         }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
