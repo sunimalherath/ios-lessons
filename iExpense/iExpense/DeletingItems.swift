7da184821263e47ddffdd281cba4759769a314c2
Sunimal Herath <sunimal@live.com>
2024-06-01 22:22:17 +1000

    Adding rows to a list

---
+++ iExpense/iExpense/DeletingItems.swift
@@ -0,0 +1,32 @@
+//
+//  DeletingItems.swift
+//  iExpense
+//
+//  Created by Sunimal Herath on 1/6/2024.
+//
+
+import SwiftUI
+
+struct DeletingItems: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("Row \($0)")
+                }
+            }
+            
+            Button("Add Number") {
+                numbers.append(currentNumber)
+                currentNumber += 1
+            }
+        }
+    }
+}
+
+#Preview {
+    DeletingItems()
+}
