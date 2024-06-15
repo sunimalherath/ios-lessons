af59bbced6077a27b6b74babad5f15086ecc09a7
Sunimal Herath <sunimal@live.com>
2024-06-15 16:16:44 +1000

    passing closure as a parameter

--- construct.playground/Contents.swift
+++ construct.playground/Contents.swift
@@ -25,3 +25,24 @@ let x = arr.sorted(by: { (name1: String, name2: String) -> Bool in
 })
 
 print(x)
+
+// -------------------------------------------------------------------
+
+func makeArray(of size: Int, using generator: ()->Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
+
+let arr2 = makeArray(of: 10) {
+    Int.random(in: 1...50)
+}
+
+print(arr2)
+
+// -------------------------------------------------------------------
