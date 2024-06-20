0d85305870c6778e680dc6f9f08cd178ae6441f8
Sunimal Herath <sunimal@live.com>
2024-06-20 22:34:47 +1000

    refactoring view model

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
-        "⚓️"
+    static let emojis = ["🚗", "🚀", "✈️", "🚌", "⛴️", "🚁", "🚣‍♂️", "🚴‍♂️", "🚢", "🚊", "🚡", "🛩️", "🛻", "🛵", "🚎", "🏎️", "🚓", "🚒", "🛸", "🛰️", "🚔", "🚘"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            EmojiMemoryGame.emojis[pairIndex]
+        }
     }
+    
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
