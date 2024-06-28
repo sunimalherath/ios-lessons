eb0d8dceb0866dd7b8841d794f2c30fe251ef4c6
Sunimal Herath <sunimal@live.com>
2024-06-28 23:00:36 +1000

    simplifying saving Userdefaults with @AppStorage

--- iExpense/iExpense/UserDefaultsView.swift
+++ iExpense/iExpense/UserDefaultsView.swift
@@ -19,6 +19,16 @@ struct UserDefaultsView: View {
     }
 }
 
+struct SimplifiedView: View {
+    @AppStorage("tapCount") private var tapCount = 0
+    var body: some View {
+        Button("Tap Count \(tapCount)") {
+            tapCount += 1
+        }
+    }
+}
+
 #Preview {
-    UserDefaultsView()
+//    UserDefaultsView()
+    SimplifiedView()
 }
