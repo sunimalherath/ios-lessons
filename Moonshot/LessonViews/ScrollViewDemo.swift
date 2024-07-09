f03f48ea576d83263e88577b992dfeb496b4b56c
Sunimal Herath <sunimal@live.com>
2024-07-09 23:22:42 +1000

    adding a custom text view

--- Moonshot/LessonViews/ScrollViewDemo.swift
+++ Moonshot/LessonViews/ScrollViewDemo.swift
@@ -12,12 +12,25 @@ struct ScrollViewDemo: View {
         ScrollView {
             VStack(spacing: 10) {
                 ForEach(0..<100) {
-                    Text("Item \($0)")
+                    CustomText("Item \($0)")
                         .font(.title)
                 }
             }
             .frame(maxWidth: .infinity)
         }
+    }
+}
+
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
     }
 }
 
