4a58ff072fa63bb347e75ef1214c08de5cef6b44
Sunimal Herath <sunimal@live.com>
2024-07-13 23:34:33 +1000

    adding extra code to diagnose the the problem if any

--- Moonshot/Moonshot/Bundle-Decodable.swift
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Bundle {
     func decode(_ file: String) -> [String: Astronaut] {
-        guard let url = self.url(forResource: file, withExtension: nil) {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
         
@@ -19,11 +19,25 @@ extension Bundle {
         
         let decoder = JSONDecoder()
         
-        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
-            fatalError("Failed to decode \(file) from bundle")
+//        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+//            fatalError("Failed to decode \(file) from bundle")
+//        }
+        
+        do {
+            return try decoder.decode([String: Astronaut].self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' – \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON.")
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
         }
         
-        return loaded
+//        return loaded
     }
 }
 
