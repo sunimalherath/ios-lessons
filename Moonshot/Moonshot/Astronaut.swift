e26c47e76e9d1f64aef0a5e1d6212f31d6b1f4cb
Sunimal Herath <sunimal@live.com>
2024-07-13 23:19:26 +1000

    adding a struct astronauts

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Sunimal Herath on 13/7/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
+
