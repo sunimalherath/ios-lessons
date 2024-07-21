cb2147a364c9d41dbcaae8e6ac7889807efb5d9a
Sunimal Herath <sunimal@live.com>
2024-07-21 11:53:06 +1000

    adding mission detail view

---
+++ Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,43 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Sunimal Herath on 21/7/2024.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+            .navigationTitle(mission.displayName)
+            .navigationBarTitleDisplayMode(.inline)
+            .background(.darkBackground)
+        }
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
